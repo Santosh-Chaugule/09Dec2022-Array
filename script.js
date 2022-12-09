74213b434b896fa9c3f52430da48fd96c29abadb
santosh-chaugule <santoshchaugule1697@gmail.com>
2022-12-09 20:23:40 +0530

    JS

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hi 'Santosh'");
+console.log('hi "Santosh"');
+console.log(`hi "Yogesh"`);
+console.log(`hi 'Yogesh'`);
+console.log('hi \'Onkar \'')//escape character
+
+console.log("HI\"Virag\"")//escape character
+
+console.log("/\\/\\/\\/\\")
